9b2450cf076239a583c2fbdfe87bf5b26b752430
Chris Coluccio <coluccioc@gmail.com>
2025-02-22 13:39:44 -0700

    Created basic folder and file structure

---
+++ include/UI.hpp
@@ -0,0 +1,19 @@
+//#ifndef UI_HPP
+#define UI_HPP
+
+/*
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QTableWidget>
+#include <vector>
+#include "BudgetManager.hpp"  // Include budget manager class
+
+
+class UI : public QWidget {
+    Q_OBJECT
+}
+
+*/
